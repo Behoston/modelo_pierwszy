85a17ec9c74fa52cbee69304ca1ab3ef1bf28596
Behoston <mlegiecki@gmail.com>
2016-03-23 08:36:51 +0100

    poprawione zapisywanie zrzutu

--- domain/Atom.py
+++ domain/Atom.py
@@ -52,20 +52,28 @@ class Atom:
         self.kinetic_energy = (self.velocity.length() * self.mass) ** 2
 
     def to_pdb_line(self):
-        id = str(self.id).ljust(5)
-        name = 'C   '
-        altLoc = ' '
-        resName = 'GLY'
-        chain = 'A'
-        x = str(round(self.coordinates[0], 5))[:8].ljust(8)
-        y = '0'.ljust(8)
-        z = '0'.ljust(8)
+        section = 'ATOM'.ljust(6)  # 1-6
+        id = str(self.id).rjust(5)  # 7-11
+        space1 = ''.rjust(1)  # 12
+        name = ' C'.ljust(4)  # 13-16
+        altLoc = ''.rjust(1)  # 17
+        resName = 'GLY'.rjust(3)  # 18-20
+        space2 = ''.rjust(1)  # 21
+        chain = 'A'.rjust(1)  # 22
+        resSeq = '1'.rjust(4)  # 23-26
+        iCode = ''.rjust(1)  # 27
+        space3 = ''.rjust(3)  # 28-30
+        x = str(round(self.coordinates[0], 5))[:8].rjust(8)  # 31-38
+        y = '0'.rjust(8)  # 39-46
+        z = '0'.rjust(8)  # 47-54
         if self.dim > 1:
-            y = str(round(self.coordinates[1], 8))[:8].ljust(8)
+            y = str(round(self.coordinates[1], 8))[:8].rjust(8)
             if self.dim > 2:
-                z = str(round(self.coordinates[2], 8))[:8].ljust(8)
-        occupancy = ' ' * 6
-        tempFactor = ' ' * 6
-        element = ' C'
-        charge = '0 '
-        return 'ATOM  ' + id + name + altLoc + resName + chain + x + y + z + occupancy + tempFactor + element + charge
+                z = str(round(self.coordinates[2], 8))[:8].rjust(8)
+        occupancy = ''.rjust(6)  # 55-60
+        tempFactor = ''.rjust(6)  # 61-66
+        space4 = ''.rjust(10)  # 67-76
+        element = 'C'.rjust(2)  # 77-78
+        charge = '0'.rjust(2)  # 79-80
+        return section + id + space1 + name + altLoc + resName + space2 + chain + resSeq + iCode + space3 + x + y + z \
+               + occupancy + tempFactor + space4 + element + charge
